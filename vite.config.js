e0df90af89d144905a2e25727e4a5e656a98cd4c
deep-learner-msp <mspraj00@gmail.com>
2024-09-15 01:11:19 +0530

    mira voice

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 
+// Vite configuration
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Allows network access if needed
-    port: 5173, // Ensure the port is specified and not in conflict
-    open: true, // Automatically opens the browser when running the dev server
-  },
+  plugins: [
+    react(),
+    visualizer({ open: true }), // Visualizer to inspect build output size
+  ],
   build: {
-    outDir: 'dist', // Default output folder for production build
+    // Optimize build output
+    minify: 'terser', // Use Terser for minification
+    chunkSizeWarningLimit: 500, // Set the chunk size warning limit (in KB)
+    rollupOptions: {
+      output: {
+        // Manual chunking to separate vendor libraries
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Adjust based on your dependencies
+        },
+      },
+    },
   },
 });
