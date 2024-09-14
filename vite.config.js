fdbb93908ef788872e0435fe20579bc119244384
deep-learner-msp <mspraj00@gmail.com>
2024-09-15 01:40:52 +0530

    fixed chunk issues

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import visualizer from 'rollup-plugin-visualizer';
+import { visualizer } from '@rollup/plugin-visualizer'; // Import from @rollup/plugin-visualizer
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true }),
+    visualizer({ open: true }), // Call visualizer as a function
   ],
   build: {
-    minify: 'terser', // Ensure terser is used for minification
+    minify: 'terser',
     chunkSizeWarningLimit: 500,
     rollupOptions: {
       output: {
