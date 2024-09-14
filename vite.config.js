15ee2291769ca4f168babbc181c2e01036a1dae1
deep-learner-msp <mspraj00@gmail.com>
2024-09-15 01:38:01 +0530

    fixed chunk issues

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { visualizer } from 'rollup-plugin-visualizer';
+import visualizer from 'rollup-plugin-visualizer';
 
-// Vite configuration
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true }), // Visualizer to inspect build output size
+    visualizer({ open: true }),
   ],
   build: {
-    // Optimize build output
-    minify: 'terser', // Use Terser for minification
-    chunkSizeWarningLimit: 500, // Set the chunk size warning limit (in KB)
+    minify: 'terser', // Ensure terser is used for minification
+    chunkSizeWarningLimit: 500,
     rollupOptions: {
       output: {
-        // Manual chunking to separate vendor libraries
         manualChunks: {
-          vendor: ['react', 'react-dom'], // Adjust based on your dependencies
+          vendor: ['react', 'react-dom'],
         },
       },
     },
